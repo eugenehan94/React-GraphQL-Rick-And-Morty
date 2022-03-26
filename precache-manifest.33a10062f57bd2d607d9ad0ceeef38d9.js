2ff07fd7bdcf7c2f21b95c71641898c12b4a79ea
eugenehan94 <eugene.han94@gmail.com>
2022-03-25 21:12:48 -0400

    Updates

---
+++ precache-manifest.33a10062f57bd2d607d9ad0ceeef38d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8b1b010d1213df2cc70",
+    "url": "/React-GraphQL-Rick-And-Morty/static/js/main.f8b1b010.chunk.js"
+  },
+  {
+    "revision": "f2c462198f751fcf8ce4",
+    "url": "/React-GraphQL-Rick-And-Morty/static/js/1.f2c46219.chunk.js"
+  },
+  {
+    "revision": "35c9f59a232401961403",
+    "url": "/React-GraphQL-Rick-And-Morty/static/js/runtime~main.35c9f59a.js"
+  },
+  {
+    "revision": "d4d52e9b075ef8a22c5067f8ba282bf2",
+    "url": "/React-GraphQL-Rick-And-Morty/index.html"
+  }
+];
